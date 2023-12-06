79fec6483b85bbcd95603b15661d024c676e8b94
hasina.md <124174359+hasina-md@users.noreply.github.com>
2023-12-06 16:40:57 +0000

    installed prisma with nextJs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nexrJs-prisma
+### next we included prisma / we should go to our folder where project is being setUp
+- npx prisma init
+### next we will include a model in schema.prisma/ to check the connection whether it is established or not
+- npx prisma studio
+## to establish connection b/w DB to prisma we should enter the below command
+- npx prisma migrate dev
+### to run nextJs application we need to run the below command
+- npm run dev
---
+++ web-development-courses/app/page.tsx
@@ -0,0 +1,27 @@
+ import { PrismaClient } from "@prisma/client";
+
+ export async function getData () {
+  const db = new PrismaClient();
+  const data = await db.course.findMany({});
+  return {
+    courses: data
+
+  }
+ 
+ }
+
+
+
+// this is the presentation layer
+export  default async function Home () {
+  const courseList =await getData();
+  return (    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2> Welcome to Next-JS with prisma included </h2>
+
+      <pre>{JSON.stringify(courseList, null, 2)}</pre>
+     
+
+    </main>
+  )
+}
